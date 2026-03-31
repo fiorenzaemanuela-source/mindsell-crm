5ee146df9ec98f07b4c2c98c065b3c8e279aaf22
fiorenzaemanuela-source <fiorenzaemanuela@gmail.com>
2026-03-31 11:03:45 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth'
 
 
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
